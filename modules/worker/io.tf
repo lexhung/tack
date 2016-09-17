366fd9f160509da2e57597569a99badd417a583e
🐨♥️🍻 <jono@kz8s.io>
2016-09-16 17:58:20 -0700

    added - terraform output
    - 'autoscaling-group-name'
    - 'name'

--- modules/worker/io.tf
+++ modules/worker/io.tf
@@ -27,4 +27,5 @@ variable "volume_size" {
 }
 variable "vpc-id" {}
 
+output "autoscaling-group-name" { value = "${ aws_autoscaling_group.worker.name }" }
 output "depends-id" { value = "${ null_resource.dummy_dependency.id }" }
