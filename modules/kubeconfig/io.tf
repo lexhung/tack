d6b1bccc57f67a70967ff9033f968d28c8e35a7f
Hung X. Le <lexhung@gmail.com>
2017-01-20 20:29:57 +0700

    Fix kubeconfig variable for dir-tmp

--- modules/kubeconfig/io.tf
+++ modules/kubeconfig/io.tf
@@ -3,6 +3,6 @@ variable "admin-pem" {}
 variable "ca-pem" {}
 variable "master-elb" {}
 variable "name" {}
-variable "build-dir" {}
+variable "dir-tmp" {}
 
 output "kubeconfig" { value = "${ data.template_file.kubeconfig.rendered }" }
